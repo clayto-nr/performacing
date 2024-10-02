5f5195a58e967f2d82ddc25c9af3be937b76f80d
clayto-nr <rton.dev@gmail.com>
2024-10-02 11:06:45 -0300

    update go

--- go/main.go
+++ go/main.go
@@ -12,7 +12,6 @@ import (
 
 var DB *gorm.DB
 
-// Modelo de usuário
 type User struct {
 	gorm.Model
 	Name     string `json:"name"`
@@ -21,32 +20,24 @@ type User struct {
 }
 
 func main() {
-	// Configuração de conexão com o MySQL
-	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local" // Sem senha
+	dsn := "root:@tcp(127.0.0.1:3306)/user_registration?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Não foi possível conectar ao banco de dados:", err)
 	}
 
-	// Migrar o modelo de usuário para a base de dados
 	DB.AutoMigrate(&User{})
 
 	r := gin.Default()
 
-	// Rota para registro de usuário
 	r.POST("/register", register)
-
-	// Rota para login de usuário
 	r.POST("/login", login)
-
-	// Rota para listar todos os usuários (apenas nome)
 	r.GET("/users", listUsers)
 
-	r.Run(":8080") // Roda na porta 8080
+	r.Run(":8080")
 }
 
-// Função de registro
 func register(c *gin.Context) {
 	var user User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -54,14 +45,12 @@ func register(c *gin.Context) {
 		return
 	}
 
-	// Verifica se o email já existe
 	var existingUser User
 	if err := DB.Where("email = ?", user.Email).First(&existingUser).Error; err == nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Email já registrado"})
 		return
 	}
 
-	// Hash da senha
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao criar senha"})
@@ -69,7 +58,6 @@ func register(c *gin.Context) {
 	}
 	user.Password = string(hashedPassword)
 
-	// Salva o usuário no banco de dados
 	if err := DB.Create(&user).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -78,7 +66,6 @@ func register(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Usuário registrado com sucesso!"})
 }
 
-// Função de login
 func login(c *gin.Context) {
 	var loginData User
 	if err := c.ShouldBindJSON(&loginData); err != nil {
@@ -92,17 +79,15 @@ func login(c *gin.Context) {
 		return
 	}
 
-	// Verifica a senha
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginData.Password))
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Login realizado com sucesso!"})
+	c.JSON(http.StatusOK, gin.H{"message": "Login realizado com sucesso"})
 }
 
-// Função para listar todos os usuários (apenas o nome)
 func listUsers(c *gin.Context) {
 	var users []User
 	DB.Select("name").Find(&users)
